e4f6b2c4bd74c586345ba4839831e2057fa23cdb
dakshh545 <dakshkochar545@gmail.com>
2026-03-18 12:50:21 +0530

    Add files via upload

---
+++ scripts/bronze/check_tables_bronze.sql
@@ -0,0 +1,23 @@
+SELECT COUNT(*) FROM crm_cust_info;
+
+SELECT COUNT(*) FROM crm_prd_info;
+
+SELECT COUNT(*) FROM crm_sales_details;
+
+SELECT COUNT(*) FROM erp_cust_az12;
+
+SELECT COUNT(*) FROM erp_loc_a101;
+
+SELECT COUNT(*) FROM erp_px_cat_g1v2;
+
+SELECT * FROM crm_cust_info LIMIT 1000;
+
+SELECT * FROM crm_prd_info LIMIT 1000;
+
+SELECT * FROM crm_sales_details LIMIT 1000;
+
+SELECT * FROM erp_cust_az12 LIMIT 1000;
+
+SELECT * FROM erp_loc_a101 LIMIT 1000;
+
+SELECT * FROM erp_px_cat_g1v2 LIMIT 1000;
